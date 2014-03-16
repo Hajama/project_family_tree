3efc2daa808479c9041598b0ffde39921a62c905
Hajama <ASASIS1@gmail.com>
2014-03-16 22:07:46 +0200

    SHARINU

--- Medis/Program.cs
+++ Medis/Program.cs
@@ -17,6 +17,7 @@ namespace Medis
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            //SHARINU
         }
     }
 }

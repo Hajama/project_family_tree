03bc813fe3e16a3387399f748efc9c1aba46b13b
Hajama <ASASIS1@gmail.com>
2014-03-16 22:09:09 +0200

    DU syk

--- Medis/Program.cs
+++ Medis/Program.cs
@@ -18,6 +18,7 @@ namespace Medis
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
             //SHARINU
+            //DU syk
         }
     }
 }
